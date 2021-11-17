6998453246dcb76dc9c05512232b82dd1f4e4e0c
sbx_user1051 <sbx_user1051@169.254.82.189>
2021-11-17 19:07:59 +0000

    Wed Nov 17 19:07:59 UTC 2021

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,8 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
+    """Just prints Hello, World! and ignores the argument for now"""
     return "Hello, World!"
+
 
 
--- test/solution_tests/HLO/test_sum.py
+++ test/solution_tests/HLO/test_sum.py
@@ -3,5 +3,8 @@ import solutions.HLO as HLO
 
 class TestSum():
     def test_sum(self):
+        help(HLO)
+        print(type(HLO.hello_solution))
         assert HLO.hello() == "Hello, World!"
+
 
