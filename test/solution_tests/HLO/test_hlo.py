404305e7e3914e316056ab8b92572d77a36123b8
sbx_user1051 <sbx_user1051@169.254.82.189>
2021-11-17 19:10:59 +0000

    Wed Nov 17 19:10:59 UTC 2021

---
+++ test/solution_tests/HLO/test_hlo.py
@@ -0,0 +1,9 @@
+import solutions.HLO as HLO
+
+
+class TestHlo():
+    def test_hlo(self):
+        #help(HLO)
+        #print(type(HLO.hello_solution))
+        #assert HLO.hello() == "Hello, World!"
+        pass
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,4 +3,7 @@ from solutions.SUM import sum_solution
 
 class TestSum():
     def test_sum(self):
+        help(solutions.sum)
+        assert False
         assert sum_solution.compute(1, 2) == 3
+
