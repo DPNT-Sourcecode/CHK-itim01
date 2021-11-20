037534110eac7068270e2b8b70e5f7547270dd6f
sbx_user1051 <sbx_user1051@169.254.113.109>
2021-11-20 16:16:00 +0000

    Sat Nov 20 16:16:00 UTC 2021

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,28 +4,28 @@ def skuString(quantities):
     s = ""
     for sku in quantities:
         s += sku * quantities[sku]
+    return s
 
 class TestChk():
 
-    def test_chk(self):
+    def test_empty(self):
         assert checkout_solution.checkout('') == 0
 
-    # TODO: Figure out how to have more than one unit test in python
-
-    def invalidItem(self):
+    def test_invalidItem(self):
         assert checkout_solution.checkout('AB9') == checkout_solution.ERROR_INVALID_ARGUMENT
 
-    def singleItem(self):
+    def test_singleItem(self):
         item = 'B'
         assert checkout_solution.checkout(item) == checkout_solution.normalPrices[item]
 
-    def oneOfEach(self):
+    def test_oneOfEach(self):
         items = checkout_solution.normalPrices.keys()
         expected = sum([checkout_solution[sku] for sku in checkout_solution.normalPrices])
         assert checkout_solution.checkout(''.join(items)) == expected
 
-    def oneOffer(self):
+    def test_oneOffer(self):
         offerContents = {'A': 1}
         offerPrice = 10
         offer = checkout_solution.MultiPriceOffer(offerContents, offerPrice)
-        assert checkout_solution.checkout(skuString(offerContents), offerPrice)
+        assert checkout_solution.checkout(skuString(offerContents)) == offerPrice
+
