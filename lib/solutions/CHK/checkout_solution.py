63e4a4260b3df138017fe72c989a61e451ad5746
sbx_user1051 <sbx_user1051@169.254.113.109>
2021-11-20 14:55:00 +0000

    Sat Nov 20 14:55:00 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,21 @@
+#!/usr/bin/python3
 
+normalPrices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    totalPrice = 0
+    for sku in skus:
+        normalPrice = normalPrices.get(sku)
+        if (normalPrice == None):
+            return -1
+        totalPrice += normalPrice
+    # TODO offers
+    return totalPrice
+
---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,8 @@
+from solutions.HLO import hello_solution
+
+
+class TestHlo():
+    def test_hlo(self):
+        assert hello_solution.hello("World") == "Hello, World!"
+        assert hello_solution.hello("Friend") == "Hello, Friend!"
+
