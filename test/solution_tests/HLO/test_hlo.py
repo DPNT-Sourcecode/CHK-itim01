dcad7ea67c54a9d4b131106262f798e7c48ddffb
sbx_user1051 <sbx_user1051@169.254.113.109>
2021-11-20 15:55:00 +0000

    Sat Nov 20 15:55:00 UTC 2021

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -6,3 +6,36 @@ class TestHlo():
         assert hello_solution.hello("World") == "Hello, World!"
         assert hello_solution.hello("Friend") == "Hello, Friend!"
 
+
+
+from solutions.CHK import checkout_solution
+
+def skuString(quantities):
+    s = ""
+    for sku in quantities:
+        s += sku * quantities[sku]
+
+class TestChk():
+    
+    def noItems(self):
+        assert checkout_solution.checkout('') == 0
+
+    def invalidItem(self):
+        assert checkout_solution.checkout('AB9') == checkout_solution.ERROR_INVALID_ARGUMENT
+
+    def singleItem(self):
+        item = 'B'
+        assert checkout_solution.checkout(item) == checkout_solution.normalPrices[item]
+
+    def oneOfEach(self):
+        items = checkout_solution.normalPrices.keys()
+        expected = sum([checkout_solution[sku] for sku in checkout_solution.normalPrices])
+        assert checkout_solution.checkout(''.join(items)) == expected
+
+    def oneOffer(self):
+        offerContents = {'A': 1}
+        offerPrice = 10
+        offer = checkout_solution.MultiPriceOffer(offerContents, offerPrice)
+        assert checkout_solution.checkout(skuString(offerContents), offerPrice)
+
+
