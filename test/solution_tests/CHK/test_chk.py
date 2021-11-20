e01d023fee666a39cdee0e6a8172f20f0019fda0
sbx_user1051 <sbx_user1051@169.254.113.109>
2021-11-20 15:07:00 +0000

    Sat Nov 20 15:07:00 UTC 2021

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -9,4 +9,12 @@ class TestChk():
     def invalidItem(self):
         assert checkout_solution.checkout('AB9') == checkout_solution.ERROR_INVALID_ARGUMENT
 
+    def singleItem(self):
+        item = 'B'
+        assert checkout_solution.checkout(item) == checkout_solution.normalPrices[item]
+
+    def oneOfEach(self):
+        items = checkout_solution.normalPrices.keys()
+        expected = sum([checkout_solution[sku] for sku in checkout_solution.normalPrices])
+        assert checkout_solution.checkout(''.join(items)) == expected
 
