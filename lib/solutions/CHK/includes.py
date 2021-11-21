1a99720b2bc687c70d008ea1d6ef3d80110b041b
sbx_user1051 <sbx_user1051@169.254.13.25>
2021-11-21 15:53:40 +0000

    Sun Nov 21 15:53:40 UTC 2021

--- lib/solutions/CHK/includes.py
+++ lib/solutions/CHK/includes.py
@@ -11,7 +11,7 @@ PRICES = {
     'H': 10,
     'I': 35,
     'J': 60,
-    'K': 80,
+    'K': 70,
     'L': 90,
     'M': 15,
     'N': 40,
@@ -19,16 +19,17 @@ PRICES = {
     'P': 50,
     'Q': 30,
     'R': 50,
-    'S': 30,
+    'S': 20,
     'T': 20,
     'U': 40,
     'V': 50,
     'W': 20,
-    'X': 90,
-    'Y': 10,
-    'Z': 50,
+    'X': 17,
+    'Y': 20,
+    'Z': 21,
 }
 
 def getTotalPrice(quantities, prices=PRICES):
     return sum(prices[sku] * quantities[sku] for sku in quantities)
+
 
